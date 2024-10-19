b9b3a67f61aac3f65534c53ced4405e1c59a2fcd
Krasov <artkrasov@gmail.com>
2024-10-19 23:29:55 +0300

    Добавьте файлы проекта.

---
+++ OnlineShop.WebApi/Program.cs
@@ -0,0 +1,12 @@
+using OnlineShop.WebApi;
+
+var host = CreateHostBuilder(args)
+           .Build();
+host.Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
---
+++ OnlineShop.WebApi/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace OnlineShop.WebApi;
+
+public class Startup(IConfiguration configuration)
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseHttpsRedirection();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
